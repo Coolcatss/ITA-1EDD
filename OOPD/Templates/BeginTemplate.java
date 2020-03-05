90fb5d8f7df9fbbc57f4f6a7542d0e189942c65a
Julian <60609449+Julianvank@users.noreply.github.com>
2020-03-05 15:42:06 +0100

    Een Template voor Processing

---
+++ OOPD/Templates/BeginTemplate.java
@@ -0,0 +1,16 @@
+import processing.core.PApplet;
+
+public class App extends PApplet {
+
+	public static void main(String[] args) {
+	}
+
+	public void settings() {
+	}
+	
+	public void setup() {
+		}
+    
+	public void draw() {
+		}
+}
