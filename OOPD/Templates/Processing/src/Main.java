d69443a40a2daac00d5804481b5fe6901b96f546
NXTTX <60346273+nxttx@users.noreply.github.com>
2020-03-05 15:43:09 +0100

    Added processing template

---
+++ OOPD/Templates/Processing/src/Main.java
@@ -0,0 +1,21 @@
+import processing.core.PApplet;
+
+public class Main extends PApplet {
+
+    // The argument passed to main must match the class name
+    public static void main(String[] args) {
+        PApplet.main("Main"); //Verander deze naam naar de hoofdclasse.
+    }
+
+    public void settings(){
+        size(500, 500);
+    }
+
+    public void setup(){
+    	
+    }
+
+    public void draw(){
+    	
+    }
+}
